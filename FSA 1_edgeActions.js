8e7c7af3c563653353657071c70f47a7ae558ddc
zbatty <job1585@gmail.com>
2014-08-19 17:39:19 -0600

    v7

--- FSA 1_edgeActions.js
+++ FSA 1_edgeActions.js
@@ -1,6 +1,16 @@
 
 (function($,Edge,compId){var Composition=Edge.Composition,Symbol=Edge.Symbol;
 //Edge symbol: 'stage'
-(function(symbolName){})("stage");
+(function(symbolName){Symbol.bindElementAction(compId,symbolName,"${_Dashboard}","click",function(sym,e){window.open("http://fsastockexchange.com/welcome-page","_self");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Portfolio}","click",function(sym,e){window.open("http://fsastockexchange.com/fantasy_stocks/portfolio","_self");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Search}","click",function(sym,e){window.open("http://fsastockexchange.com/manager/listStocks","_self");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Buy_Credits}","click",function(sym,e){window.open("http://fsastockexchange.com/products","_self");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Account_Settings}","click",function(sym,e){window.open("http://fsastockexchange.com/user","_self");});
+//Edge binding end
+})("stage");
 //Edge symbol end:'stage'
 })(jQuery,AdobeEdge,"EDGE-92559202");
