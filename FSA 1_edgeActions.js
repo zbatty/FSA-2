afde84bca7c7b03c9191d81b106d25ed394d2f61
zbatty <job1585@gmail.com>
2014-08-19 23:02:49 -0600

    v7

--- FSA 1_edgeActions.js
+++ FSA 1_edgeActions.js
@@ -1,15 +1,15 @@
 
 (function($,Edge,compId){var Composition=Edge.Composition,Symbol=Edge.Symbol;
 //Edge symbol: 'stage'
-(function(symbolName){Symbol.bindElementAction(compId,symbolName,"${_Dashboard}","click",function(sym,e){window.open("http://fsastockexchange.com/welcome-page","_self");});
+(function(symbolName){Symbol.bindElementAction(compId,symbolName,"${_Dashboard}","click",function(sym,e){window.open("http://fsastockexchange.com/welcome-page","_system");});
 //Edge binding end
-Symbol.bindElementAction(compId,symbolName,"${_Portfolio}","click",function(sym,e){window.open("http://fsastockexchange.com/fantasy_stocks/portfolio","_self");});
+Symbol.bindElementAction(compId,symbolName,"${_Portfolio}","click",function(sym,e){window.open("http://fsastockexchange.com/fantasy_stocks/portfolio","_top");});
 //Edge binding end
-Symbol.bindElementAction(compId,symbolName,"${_Search}","click",function(sym,e){window.open("http://fsastockexchange.com/manager/listStocks","_self");});
+Symbol.bindElementAction(compId,symbolName,"${_Search}","click",function(sym,e){window.open("http://fsastockexchange.com/manager/listStocks","_system");});
 //Edge binding end
-Symbol.bindElementAction(compId,symbolName,"${_Buy_Credits}","click",function(sym,e){window.open("http://fsastockexchange.com/products","_self");});
+Symbol.bindElementAction(compId,symbolName,"${_Buy_Credits}","click",function(sym,e){window.open("http://fsastockexchange.com/products","_blank");});
 //Edge binding end
-Symbol.bindElementAction(compId,symbolName,"${_Account_Settings}","click",function(sym,e){window.open("http://fsastockexchange.com/user","_self");});
+Symbol.bindElementAction(compId,symbolName,"${_Account_Settings}","click",function(sym,e){window.open("http://fsastockexchange.com/user","_parent");});
 //Edge binding end
 })("stage");
 //Edge symbol end:'stage'
